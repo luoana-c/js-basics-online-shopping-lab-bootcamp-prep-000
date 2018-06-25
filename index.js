43c8ca77344d44a9f9a624e1cfc3f3bbe539a24e
Luoana Chirita <luoana.livadariu@gmail.com>
2018-06-25 11:48:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ function findItem(itemName){
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber == ''){
+  if (!cardNumber){
     return "Sorry, we don't have a credit card on file for you.";
   }
   var pay = total();
